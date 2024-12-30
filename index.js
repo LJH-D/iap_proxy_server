bb266408705b5fa7f8324168cbb7be7dbb2bfa9c
LJH-D <developer2@dlnl.co.kr>
2024-12-30 17:25:15 +0900

    zdw

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
 const PORT = 8080;
 
 const app = express();
@@ -9,19 +10,9 @@ app.use(express.json());
 
 app.post('/', async (req, res) => {
   console.log('Received Apple In-App Purchase Webhook:');
-  console.log(JSON.stringify(req.body, null, 2)); // 웹훅 데이터를 콘솔에 출력
-
-  // 예: 데이터 검증 또는 처리 로직 추가
-  if (req.body && req.body.notification_type) {
-    console.log(`Notification Type: ${req.body.notification_type}`);
-  }
+  console.log(jwt.verify(req.body.signedPayload));
   await axios.post('http://15.164.123.51:18080/v1/receipts/app-store-notifications', req.body);
-  // Apple의 웹훅은 상태 코드 200을 반환해야 함
   res.status(200).send('Webhook received');
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
