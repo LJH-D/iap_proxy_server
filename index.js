6addcd19f6bf57fea67afeb8ab65f82b9fea521e
LJH-D <developer2@dlnl.co.kr>
2024-12-30 16:53:21 +0900

    zz

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const axios = require('axios');
 const PORT = 8080;
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 // JSON 본문을 파싱하기 위한 미들웨어
 app.use(express.json());
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   console.log('Received Apple In-App Purchase Webhook:');
   console.log(JSON.stringify(req.body, null, 2)); // 웹훅 데이터를 콘솔에 출력
 
@@ -15,7 +15,7 @@ app.post('/', (req, res) => {
   if (req.body && req.body.notification_type) {
     console.log(`Notification Type: ${req.body.notification_type}`);
   }
-
+  await axios.post('http://15.164.123.51:18080/v1/receipts/app-store-notifications', req.body);
   // Apple의 웹훅은 상태 코드 200을 반환해야 함
   res.status(200).send('Webhook received');
 });
